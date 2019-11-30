f440187677ea6d31846fd0096558e73b37790d6f
jeremyspartan <jyvebuz@gamil.com>
2019-11-30 11:26:10 -0600

    obtener tecla

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DadoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                if(Console.ReadKey().Key==ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Nueva Visualizacion");
+                }
+            } while (true);
+        }
+    }
+}
