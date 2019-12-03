add256ddb170c4084059f48d7929cf3256fe8638
jeremyspartan <jyvebuz@gmail.com>
2019-12-02 23:28:16 -0600

    Se toma una tecla de entrada para limpiar la pantalla

---
+++ DadoCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DadoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Backspace)
+                {
+                    Console.WriteLine("Limpiado");
+                }
+            } while (true);
+        }
+    }
+}
