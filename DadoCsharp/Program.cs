ce042d173305aac376baa99a8f27d1fa49f00198
jeremyspartan <jyvebuz@gmail.com>
2019-12-03 19:41:13 -0600

    agragados  los numeros aleatorios del dado

--- DadoCsharp/Program.cs
+++ DadoCsharp/Program.cs
@@ -10,30 +10,51 @@ namespace DadoCsharp
             {
                 if(Console.ReadKey().Key==ConsoleKey.Enter)
                 {
+                    Console.Clear();
                     Console.WriteLine("Nueva Visualizacion");
                 }
-                DibujarDado(4, 8, 2);
+                Random r = new Random();
+                int num = r.Next(1,7);
+                int[] arreglo = new int[3]; 
+                arreglo[0]=num;
+
+                num = r.Next(1, 7); 
+                while(arreglo[0] == num)
+                {
+                    num = r.Next(1, 7);
+                }
+                arreglo[1] = num; 
+
+                num = r.Next(1, 8);
+                while (arreglo[0] == num || arreglo[1] == num)
+                {
+                    num = r.Next(1, 7);
+                }
+                arreglo[2] = num;
+
+                DibujarDado(arreglo[0], arreglo[1], arreglo[2]);
             } while (true);
 
 
             static void DibujarDado(int CaraSuperior,int CaraFrontal,int CaraIzquierda)
             {
                 //cara superior
-                Console.WriteLine("             ########################");
-                Console.WriteLine("             #                      #");
-                Console.WriteLine("             #            {0}         #",CaraSuperior);
-                Console.WriteLine("             #                      #");
+                Console.WriteLine("                       ##############");
+                Console.WriteLine("                       #            #");
+                Console.WriteLine("                       #     {0}      #",CaraSuperior);
+                Console.WriteLine("                       #            #");
                 //cara frontal
-                Console.WriteLine("#################################################");
-                Console.WriteLine("#          #              #            #          #");
-                Console.WriteLine("#    {0}   #    {1}       #     {2}         #     {3}    #",7-CaraFrontal,7-CaraIzquierda,CaraFrontal,CaraIzquierda);
-                Console.WriteLine("#          #              #            #          #");
-                Console.WriteLine("##################################################");
+                Console.WriteLine("################################################");
+                Console.WriteLine("#            #         #            #          #");
+                Console.WriteLine("#     {0}      #    {1}    #     {2}      #     {3}    #",7-CaraFrontal,7-CaraIzquierda,CaraFrontal,CaraIzquierda);
+                Console.WriteLine("#            #         #            #          #");
+                Console.WriteLine("################################################");
                 //cara ziquierda
-                Console.WriteLine("            #                      #");
-                Console.WriteLine("            #            {0}         #", CaraSuperior);
-                Console.WriteLine("            #                      #");
-                Console.WriteLine("            ########################");
+                Console.WriteLine("                       #            #");
+                Console.WriteLine("                       #     {0}      #", 7-CaraSuperior);
+                Console.WriteLine("                       #            #");
+                Console.WriteLine("                       ##############");
+
             }
         }
     }
